a96d3375e3b2859a0d80006956401d8251d70ef8
Jacek Kuliś <jacek.jk.kulis@gmail.com>
2018-01-27 13:13:34 +0100

    adding wcf hello world project

---
+++ wcf/helloworld/HelloWorldClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using HelloWorldClient.HelloWorldServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldServiceClient client = new HelloWorldServiceClient();
+            Console.WriteLine("Write your nickname: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Waiting for response from service...");
+            Console.WriteLine(client.SayHelloWorld(name));
+            client.Close();
+            Console.ReadLine();
+        }
+    }
+}
